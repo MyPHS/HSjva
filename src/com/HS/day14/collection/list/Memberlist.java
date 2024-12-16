2536f20c12b545fcb9e05e5c9eda994b039319b7
user1 <user1@DESKTOP-Q1Q428R>
2024-12-16 12:31:27 +0900

    [ADD] Day14 컬렉션과 제네릭
    1.List: 순서를 유지하고 중복 저장될 수 있는 자료구조,
    맛집 대기자 리스트와 같은 형태
    2. Set: 순서가 중요하지 않다면 중복을 저장할 수 없음.
    집합이 그 예시이며 중복을 제거할 때 사용됨
    3. Map : 키와 밸류로 이루어진 쌍으로 데이터를 저장하며
    if문으로 구현했던 부분을 저장소로 적용하여 프로그래밍할 수 있음.
    * 제네릭: 특정타입만 다루지 않고 여러 종류의 타입을
    다룰 수 있도록 일반화시키는 기법

---
+++ src/com/HS/day14/collection/list/Memberlist.java
@@ -0,0 +1,36 @@
+package com.HS.day14.collection.list;
+class Member{
+	
+}
+public class Memberlist {
+	Member [] members;
+	int size;
+	
+	public Memberlist() {
+		members = new Member[3];
+		size = 0;
+	}
+	
+	// 추가
+	public void add(Member member) {
+		members[size] = member;
+		size++;
+		
+	}
+	// 검색(조회)
+	public Member get(int index) {
+		return members[index];
+		
+	}
+	
+	// 크기
+	public int size() {
+		return size;
+	}
+	
+	// 전체삭제
+	public void clear() {
+		members = new Member[3];
+		size = 0;
+	}
+}
